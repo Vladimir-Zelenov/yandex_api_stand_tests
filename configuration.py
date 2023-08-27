6eeaa31ce264487975cab84dd7e860214f256b11
Vladimir <vowa.zelenoff@gmail.com>
2023-08-27 12:49:25 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://7af564df-9b9e-4f8d-aa8c-c48afd86faa6.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
